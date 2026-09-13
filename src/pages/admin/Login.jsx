ae1c9f0a97a07faaf4c19ee6227a5f9db7236c0a
AdbrasCubatao <adbras.sedecubatao@gmail.com>
2026-09-13 01:48:12 -0300

    Import AdminLogin component in Login.jsx

--- src/pages/admin/Login.jsx
+++ src/pages/admin/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../../lib/supabaseClient.js'
+import AdminLogin from './pages/admin/Login.jsx'
 
 export default function Login() {
   const [email, setEmail] = useState('')
