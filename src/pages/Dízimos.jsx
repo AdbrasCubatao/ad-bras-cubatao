0ffbb468f1b9eed21d49166dc96c35d1a8f12f64
AdbrasCubatao <adbras.sedecubatao@gmail.com>
2026-09-12 23:53:24 -0300

    Create Dízimos component with maintenance message

---
+++ src/pages/Dízimos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dizimos() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h2>Dízimos e Ofertas</h2>
+      <p>Página em manutenção.</p>
+    </div>
+  )
+}
