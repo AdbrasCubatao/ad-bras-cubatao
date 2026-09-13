d6c20ccf6396274356a1092a32a17c88bee4e8eb
AdbrasCubatao <adbras.sedecubatao@gmail.com>
2026-09-12 22:44:41 -0300

    Refactor routing and import statements in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,46 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+// Importação das páginas públicas
 import Home from './pages/Home.jsx'
-import Dashboard from './pages/admin/Dashboard.jsx'
+import Agenda from './pages/Agenda.jsx'
+import Study from './pages/Study.jsx'
+import Announcements from './pages/Announcements.jsx'
+import Biblia from './pages/Biblia.jsx'
+import Departments from './pages/Departments.jsx'
+import DepartmentDetail from './pages/DepartmentDetail.jsx'
+import Dizimos from './pages/Dizimos.jsx'
+import Prayer from './pages/Prayer.jsx'
+import Quiz from './pages/Quiz.jsx'
+
+// Importação das páginas administrativas
 import AdminLogin from './pages/admin/AdminLogin.jsx'
-import ManageAgenda from './pages/admin/ManageAgenda.jsx'
-import ManageStudies from './pages/admin/ManageStudies.jsx'
-import { 
-  StudiesPage, 
-  BiblePage, 
-  CultosPage, 
-  WorshipPage, 
-  LocationPage, 
-  TithesPage, 
-  ContactsPage, 
-  MorePage 
-} from './pages/StaticPages.jsx'
+import Dashboard from './pages/admin/Dashboard.jsx'
+import AdminAgenda from './pages/admin/AdminAgenda.jsx'
+import AdminStudies from './pages/AdminStudies.jsx'
 
 export default function App() {
   return (
-    <div style={{ minHeight: '100vh', backgroundColor: '#f8fafc', paddingBottom: '70px' }}>
+    <Router>
       <Routes>
+        {/* Rotas Públicas */}
         <Route path="/" element={<Home />} />
-        <Route path="/estudos" element={<StudiesPage />} />
-        <Route path="/biblia" element={<BiblePage />} />
-        <Route path="/cultos" element={<CultosPage />} />
-        <Route path="/louvores" element={<WorshipPage />} />
-        <Route path="/localizacao" element={<LocationPage />} />
-        <Route path="/dizimos" element={<TithesPage />} />
-        <Route path="/contatos" element={<ContactsPage />} />
-        <Route path="/mais" element={<MorePage />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/estudos" element={<Study />} />
+        <Route path="/avisos" element={<Announcements />} />
+        <Route path="/biblia" element={<Biblia />} />
+        <Route path="/departamentos" element={<Departments />} />
+        <Route path="/departamentos/:id" element={<DepartmentDetail />} />
+        <Route path="/dizimos" element={<Dizimos />} />
+        <Route path="/pedidos-oracao" element={<Prayer />} />
+        <Route path="/quiz" element={<Quiz />} />
 
         {/* Rotas Administrativas */}
         <Route path="/admin/login" element={<AdminLogin />} />
         <Route path="/admin" element={<Dashboard />} />
-        <Route path="/admin/agenda" element={<ManageAgenda />} />
-        <Route path="/admin/estudos" element={<ManageStudies />} />
+        <Route path="/admin/agenda" element={<AdminAgenda />} />
+        <Route path="/admin/estudos" element={<AdminStudies />} />
       </Routes>
-
-      <Navbar />
-    </div>
+    </Router>
   )
 }
