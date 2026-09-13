442fa0a2c4de05a98c8ff5602703c47b4a10f962
AdbrasCubatao <adbras.sedecubatao@gmail.com>
2026-09-13 00:30:38 -0300

    Add Dizimos route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Announcements from './pages/Announcements.jsx'
 import Biblia from './pages/Biblia.jsx'
 import Departments from './pages/Departments.jsx'
 import DepartmentDetail from './pages/DepartmentDetail.jsx'
+import Dizimos from './pages/Dizimos.jsx'
 import Prayer from './pages/Prayer.jsx'
 import Quiz from './pages/Quiz.jsx'
 
@@ -30,6 +31,7 @@ export default function App() {
         <Route path="/biblia" element={<Biblia />} />
         <Route path="/departamentos" element={<Departments />} />
         <Route path="/departamentos/:id" element={<DepartmentDetail />} />
+        <Route path="/dizimos" element={<Dizimos />} />
         <Route path="/pedidos-oracao" element={<Prayer />} />
         <Route path="/quiz" element={<Quiz />} />
 
