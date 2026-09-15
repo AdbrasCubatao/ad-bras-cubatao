4cd081ad13964d7ed88aeb325ee3259c5ffc15e8
AdbrasCubatao <adbras.sedecubatao@gmail.com>
2026-09-15 02:50:45 -0300

    Add Link import from react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 // Páginas Públicas
 import Home from './pages/Home.jsx'
